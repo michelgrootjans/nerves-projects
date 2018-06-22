5e23969046fb3901076038ad5d74962bf8f7afe6
Michel Grootjans <michel.grootjans@gmail.com>
2018-06-22 21:39:29 +0200

    initial commit

---
+++ lib/traffic_light.ex
@@ -0,0 +1,18 @@
+defmodule TrafficLight do
+  @moduledoc """
+  Documentation for TrafficLight.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrafficLight.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/traffic_light_test.exs
@@ -0,0 +1,8 @@
+defmodule TrafficLightTest do
+  use ExUnit.Case
+  doctest TrafficLight
+
+  test "greets the world" do
+    assert TrafficLight.hello() == :world
+  end
+end
