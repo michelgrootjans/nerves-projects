e26e25f3b36132071ca53d196dbd97c44b1a1a18
Michel Grootjans <michel.grootjans@gmail.com>
2018-06-24 12:23:39 +0200

    small refactor

--- traffic_light/lib/traffic_light/blinker.ex
+++ traffic_light/lib/traffic_light/blinker.ex
@@ -12,26 +12,26 @@ defmodule Blinker do
     {:ok, yellow} = GPIO.start_link(27, :output)
     {:ok, green} = GPIO.start_link(22, :output)
 
-    lights = :queue.new()
-    lights = :queue.in(%{pid: green,  key: :green,  interval: 3_000}, lights)
-    lights = :queue.in(%{pid: yellow, key: :yellow, interval: 1_000}, lights)
-    lights = :queue.in(%{pid: red,    key: :red,    interval: 3_000}, lights)
+    cycle = :queue.new()
+    cycle = :queue.in(%{key: :green,  interval: 2_000}, cycle)
+    cycle = :queue.in(%{key: :yellow, interval: 1_000}, cycle)
+    cycle = :queue.in(%{key: :red,    interval: 2_000}, cycle)
 
-    schedule_cast(1)
-    {:ok, %{red: red, yellow: yellow, green: green, lights: lights}}
+    schedule_cycle(1)
+    {:ok, %{red: red, yellow: yellow, green: green, cycle: cycle}}
   end
 
-  def schedule_cast(time) do
-    Process.send_after(self(), :cycle_lights, time)
+  def schedule_cycle(time) do
+    Process.send_after(self(), :apply_cycle, time)
   end
 
-  def handle_info(:cycle_lights, state) do
-    {{:value, light}, other_lights} = :queue.out(state.lights)
+  def handle_info(:apply_cycle, state) do
+    {{:value, light}, other_lights} = :queue.out(state.cycle)
 
     color_me(light.key, state)
-    schedule_cast(light.interval)
+    schedule_cycle(light.interval)
 
-    {:noreply, %{state | lights: :queue.in(light, other_lights)}}
+    {:noreply, %{state | cycle: :queue.in(light, other_lights)}}
   end
 
   def color_me(color, state) do
